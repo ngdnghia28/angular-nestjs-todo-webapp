5ad0da6166f0e10681c14d5208d6ae64c9ea2dd5
ngdnghia28 <ngd.nghia28@gmail.com>
2019-06-13 15:38:47 +0700

    add todo service

---
+++ src/app/todo-data.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { Observable } from 'rxjs';
+import { Todo } from './todo';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoDataService {
+  rootPath = 'todos';
+
+  constructor(
+    private apiService: ApiService,
+  ) { }
+
+  getTodos(): Observable<any> {
+    const url = this.rootPath;
+    return this.apiService.get(url);
+  }
+
+  getTodo(todo: Todo): Observable<any> {
+    const url = this.rootPath;
+    return this.apiService.get(url);
+  }
+
+  createTodo(todo: Todo): Observable<any> {
+    const url = this.rootPath;
+    return this.apiService.post(url, todo);
+  }
+
+  updateTodo(todo: Todo): Observable<any> {
+    const url = `${this.rootPath}/${todo.id}`;
+    return this.apiService.put(url, todo);
+  }
+
+  deleteTodo(todo: Todo): Observable<any> {
+    const url = `${this.rootPath}/${todo.id}`;
+    return this.apiService.delete(url);
+  }
+}
